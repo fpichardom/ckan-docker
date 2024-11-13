90b005245d10bbcdb707cb0d0ca3111c846363cd
Fritz J. Pichardo Marcano <fpichardom@yahoo.com>
2024-11-13 21:39:58 +0000

    Add a custom entry point script to change base templates and public directory to alternative design

---
+++ ckan/docker-entrypoint.d/03_setup_custom_default_templates.sh
@@ -0,0 +1,7 @@
+ckan config-tool $CKAN_INI \
+        "ckan.base_public_folder = public-midnight-blue" \
+        "ckan.base_templates_folder = templates-midnight-blue"
+
+    echo "Custom template folder configured."
+
+
